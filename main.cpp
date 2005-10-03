5e1881fb83d90b2e3dd8b82a8ae81689883c7f66
Stephan Kulow <coolo@kde.org>
2005-10-03 17:46:54 +0000

    first round of porting
    svn path=/trunk/KDE/kdegames/kmahjongg/; revision=466923

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main( int argc, char** argv )
     KCmdLineArgs::init( argc, argv, &aboutData );
 
     KApplication a;
-    KGlobal::locale()->insertCatalogue("libkdegames");
+    KGlobal::locale()->insertCatalog("libkdegames");
     KImageIO::registerFormats();
 
     if (a.isRestored())
