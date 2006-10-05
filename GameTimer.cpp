84450ef7a2b57e972c222b516f587ac39743f599
Mauricio Piacentini <piacentini@kde.org>
2006-10-05 14:09:14 +0000

    EBN fixes, tracked origin of code from SVN. Originally licensed as GPL 2
    svn path=/trunk/KDE/kdegames/kmahjongg/; revision=592720

--- GameTimer.cpp
+++ GameTimer.cpp
@@ -1,6 +1,17 @@
-/*
- * Based upon the QT example dclock 
- */
+/* -------------------------------------------------------------------------
+   -- kmahjongg timer. Based on a slightly modified verion of the QT demo --
+   -- program dclock. Copyright as shown below.                           --
+   ------------------------------------------------------------------------- */
+
+/****************************************************************************
+** $Id$
+**
+** Copyright (C) 1992-1998 Troll Tech AS.  All rights reserved.
+**
+** This file is part of an example program for Qt.  This example
+** program may be used, distributed and modified without limitation.
+**
+*****************************************************************************/
 
 #include <stdio.h>
 #include "GameTimer.h"
