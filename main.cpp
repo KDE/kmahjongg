9ecbd5512d420e47663e57d1da4aa6c06306581e
Albert Astals Cid <tsdgeos@terra.es>
2007-09-27 20:09:51 +0000

    add the libkmahjongg catalog so the i18n contained there work
    svn path=/trunk/KDE/kdegames/kmahjongg/; revision=717968

--- main.cpp
+++ main.cpp
@@ -47,6 +47,7 @@ int main( int argc, char** argv )
 
     KApplication application;
     KGlobal::locale()->insertCatalog("libkdegames");
+    KGlobal::locale()->insertCatalog("libkmahjongg");
     
 
     if (application.isSessionRestored())
