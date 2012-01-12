a422d71be620362071b5d41cc730f220943993cd
Christian Krippendorf <coding@christian-krippendorf.de>
2012-01-12 17:17:45 +0000

    Reformatting: FrameImage.h,FrameImage.cpp
    svn path=/trunk/KDE/kdegames/kmahjongg/; revision=1273325

--- FrameImage.cpp
+++ FrameImage.cpp
@@ -1,22 +1,27 @@
-/*
-    Copyright (C) 1997 Mathias Mueller   <in5y158@public.uni-hamburg.de>
-
-    Kmahjongg is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+/* Copyright (C) 1997 Mathias Mueller   <in5y158@public.uni-hamburg.de>
+ * Copyright (C) 2006 Mauricio Piacentini   <mauricio@tabuleiro.com>
+ *
+ * Kmahjongg is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
 
 #include "FrameImage.h"
+#include "prefs.h"
+
+#include <qevent.h>
+#include <qimage.h>
+#include <qpainter.h>
+#include <qtextstream.h>
 
 #include <kcomponentdata.h>
 #include <kfiledialog.h>
@@ -26,39 +31,31 @@
 #include <kstandarddirs.h>
 #include <KStandardGuiItem>
 #include <kimageio.h>
-
-#include <qevent.h>
-#include <qimage.h>
-#include <qpainter.h>
-#include <qtextstream.h>
 #include <kvbox.h>
 
 
-#include "prefs.h"
-
-FrameImage::FrameImage (QWidget *parent, const QSize& initialImageSize)
+FrameImage::FrameImage(QWidget *parent, const QSize &initialImageSize)
   : KGameCanvasWidget(parent)
 {
-	rx = -1;
-	thePixmap = new QPixmap(initialImageSize);
+    rx = -1;
+    thePixmap = new QPixmap(initialImageSize);
 }
 
 FrameImage::~FrameImage()
 {
-	delete thePixmap;
+    delete thePixmap;
 }
 
-void FrameImage::resizeEvent( QResizeEvent* ev )
+void FrameImage::resizeEvent(QResizeEvent *ev)
 {
     *thePixmap = QPixmap(ev->size());
 }
 
-void FrameImage::paintEvent( QPaintEvent* pa )
+void FrameImage::paintEvent(QPaintEvent *pa)
 {
     //QFrame::paintEvent(pa);
 
     QPainter p(this);
-
 
     QPen line;
     line.setStyle(Qt::DotLine);
@@ -73,58 +70,59 @@ void FrameImage::paintEvent( QPaintEvent* pa )
     int h = pa->rect().height();
     int w  = pa->rect().width();
 
-    //p.drawPixmap(x+frameWidth(),y+frameWidth(),*thePixmap,x+frameWidth(),y+frameWidth(),w-(2*frameWidth()),h-(2*frameWidth()));
-p.drawPixmap(x,y,*thePixmap,x,y,w-(2),h-(2));
+//     p.drawPixmap(x+frameWidth(),y+frameWidth(),*thePixmap,x+frameWidth(),y+frameWidth(),w-(2*frameWidth()),h-(2*frameWidth()));
+    p.drawPixmap(x, y, *thePixmap, x, y, w - 2, h - 2);
     if (rx >=0) {
+        p.drawRect(rx + rs, ry, rw - rs, rh - rs);
+        p.drawLine(rx, ry + rs, rx, ry + rh);
+        p.drawLine(rx, ry + rs, rx + rs, ry);
+        p.drawLine(rx, ry + rh, rx + rs, ry + rh - rs);
+        p.drawLine(rx, ry + rh, rx + rw - rs, ry + rh);
+        p.drawLine(rx + rw - rs, ry + rh, rx + rw, ry + rh - rs);
 
-    p.drawRect(rx + rs, ry, rw - rs, rh - rs);
-    p.drawLine(rx, ry + rs, rx, ry + rh);
-    p.drawLine(rx, ry + rs, rx + rs, ry);
-    p.drawLine(rx, ry + rh, rx + rs, ry + rh - rs);
-    p.drawLine(rx, ry + rh, rx + rw - rs, ry + rh);
-    p.drawLine(rx + rw - rs, ry + rh, rx + rw, ry + rh - rs);
+        int midX = rx + rs + ((rw - rs) / 2);
+        int midY = ry + ((rh - rs) / 2);
+        switch (rt) {
+        case 0:  // delete mode cursor
+            p.drawLine(rx + rs, ry, rx + rw, ry + rh - rs);
+            p.drawLine(rx + rw, ry, rx + rs, ry + rh - rs);
 
-	int midX = rx+rs+((rw-rs)/2);
-	int midY = ry+((rh-rs)/2);
-	switch (rt) {
-		case 0:  // delete mode cursor
-			p.drawLine(rx+rs, ry, rx+rw, ry+rh-rs);
-			p.drawLine(rx+rw, ry, rx+rs, ry+rh-rs);
-		break;
-		case 1: // insert cursor
-			p.drawLine(midX, ry, midX, ry+rh-rs);
-			p.drawLine(rx+rs, midY, rx+rw, midY);
-		break;
-		case 2: // move mode cursor
-			p.drawLine(midX, ry, rx+rw, midY);
-			p.drawLine(rx+rw, midY, midX, ry+rh-rs);
-			p.drawLine(midX, ry+rh-rs, rx+rs, midY);
-			p.drawLine(rx+rs, midY, midX, ry);
+            break;
+        case 1: // insert cursor
+            p.drawLine(midX, ry, midX, ry + rh - rs);
+            p.drawLine(rx + rs, midY, rx + rw, midY);
 
-		break;
-	}
+            break;
+        case 2: // move mode cursor
+            p.drawLine(midX, ry, rx + rw, midY);
+            p.drawLine(rx + rw, midY, midX, ry + rh - rs);
+            p.drawLine(midX, ry + rh - rs, rx + rs, midY);
+            p.drawLine(rx + rs, midY, midX, ry);
 
+            break;
+        }
     }
 }
 
-void FrameImage::setRect(int x,int y,int w,int h, int s, int t)
+void FrameImage::setRect(int x, int y, int w, int h, int s, int t)
 {
-	rx = x;
-	ry = y;
-	rw = w;
-	rh = h;
-	rt = t;
-	rs = s;
+    rx = x;
+    ry = y;
+    rw = w;
+    rh = h;
+    rt = t;
+    rs = s;
 }
 
-// Pass on the mouse presed event to our owner
-
-void FrameImage::mousePressEvent(QMouseEvent *m) {
-	emit mousePressed(m);
+void FrameImage::mousePressEvent(QMouseEvent *m)
+{
+    emit mousePressed(m);
 }
 
-void FrameImage::mouseMoveEvent(QMouseEvent *e) {
-	emit mouseMoved(e);
+void FrameImage::mouseMoveEvent(QMouseEvent *e)
+{
+    emit mouseMoved(e);
 }
+
 
 #include "FrameImage.moc"
--- FrameImage.h
+++ FrameImage.h
@@ -1,32 +1,33 @@
-/*
-    Copyright (C) 1997 Mathias Mueller   <in5y158@public.uni-hamburg.de>
+/* Copyright (C) 1997 Mathias Mueller   <in5y158@public.uni-hamburg.de>
+ * Copyright (C) 2006 Mauricio Piacentini   <mauricio@tabuleiro.com>
+ *
+ * Kmahjongg is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
 
-    Kmahjongg is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+#ifndef FRAMEIMAGE_H
+#define FRAMEIMAGE_H
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
-
-#ifndef _FrameImage_H
-#define _FrameImage_H
-
-#include <kdialog.h>
-
-#include <qframe.h>
 #include "kgamecanvas.h"
-
 #include "kmahjonggtileset.h"
 #include "kmahjonggbackground.h"
 #include "BoardLayout.h"
+
+#include <qframe.h>
+
+#include <kdialog.h>
+
 
 class QComboBox;
 class QPixmap;
@@ -36,67 +37,76 @@ class QPixmap;
  * 
  * longer description
  *
- * @author Mauricio Piacentini  <mauricio@tabuleiro.com>
- */
+ * @author Mauricio Piacentini  <mauricio@tabuleiro.com> */
 class FrameImage: public KGameCanvasWidget
 {
-	Q_OBJECT
+    Q_OBJECT
+
 public:
     /**
      * Constructor
-       
-       @param parent 
-       @param initialImageSize
-    */
-	FrameImage(QWidget *parent, const QSize& initialImageSize);
+     *
+     * @param parent
+     * @param initialImageSize */
+    FrameImage(QWidget *parent, const QSize &initialImageSize);
+
     /**
      * Default Destructor */
-	~FrameImage();
+    ~FrameImage();
+
     /**
      * Method Description
      * 
-     * @return QPixmap
-     */
-	QPixmap *getPreviewPixmap() {return thePixmap;}
+     * @return QPixmap */
+    QPixmap* getPreviewPixmap() {return thePixmap;}
+
     /**
      * Method Description 
+     *
+     * @param x
+     * @param y
+     * @param w
+     * @param h
+     * @param ss
+     * @param type */
+    void setRect(int x, int y, int w, int h, int ss, int type);
 
-       @param x
-       @param y
-       @param w
-       @param h
-       @param ss
-       @param type
-     */
-	void setRect(int x, int y, int w, int h, int ss, int type);
 signals:
     /**
      * Signal Description */
-	void mousePressed(QMouseEvent *e);
+    void mousePressed(QMouseEvent *e);
+
     /**
      * Signal Description */
-	void mouseMoved(QMouseEvent *e);
+    void mouseMoved(QMouseEvent *e);
+
 protected:
     /**
      * Event Description */
-	void mousePressEvent(QMouseEvent *e);
+    void mousePressEvent(QMouseEvent *e);
+
     /**
      * Event Description */
-	void mouseMoveEvent(QMouseEvent *e);
+    void mouseMoveEvent(QMouseEvent *e);
+
     /**
      * Event Description */
-	void resizeEvent(QResizeEvent *e);
+    void resizeEvent(QResizeEvent *e);
+
     /**
      * Event Description */
-	void paintEvent( QPaintEvent* pa );
+    void paintEvent( QPaintEvent* pa );
+
 private:
-	QPixmap *thePixmap;
-	int rx;
-	int ry;
-	int rw;
-	int rh;
-	int rs;
-	int rt;
+    int rx;
+    int ry;
+    int rw;
+    int rh;
+    int rs;
+    int rt;
+
+    QPixmap *thePixmap;
 };
+
 
 #endif
