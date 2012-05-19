7d2d634545f42ed67bb2834896a83777c126906d
Christian Krippendorf <coding@christian-krippendorf.de>
2012-05-19 12:39:57 +0000

    Update the layout of the main.cpp file. (100 chars per line max and some tabs)
    svn path=/trunk/KDE/kdegames/kmahjongg/; revision=1295694

--- main.cpp
+++ main.cpp
@@ -1,18 +1,16 @@
 /* Copyright (C) 1997 Mathias Mueller   <in5y158@public.uni-hamburg.de>
  *
- * Kmahjongg is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Kmahjongg is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA. */
 
 #include "kmahjongg.h"
 #include "version.h"
@@ -29,10 +27,9 @@ static const char description[] = I18N_NOOP("Mahjongg Solitaire for KDE");
 
 int main(int argc, char** argv)
 {
-    KAboutData aboutData("kmahjongg", 0, ki18n("KMahjongg"),
-        KMAHJONGG_VERSION, ki18n(description), KAboutData::License_GPL,
-        ki18n("(c) 1997, Mathias Mueller\n(c) 2006, Mauricio Piacentini"), KLocalizedString(), "htt"
-        "p://games.kde.org/kmahjongg");
+    KAboutData aboutData("kmahjongg", 0, ki18n("KMahjongg"), KMAHJONGG_VERSION, ki18n(description),
+        KAboutData::License_GPL, ki18n("(c) 1997, Mathias Mueller\n(c) 2006, Mauricio Piacentini"),
+        KLocalizedString(), "http://games.kde.org/kmahjongg");
     aboutData.addAuthor(ki18n("Mathias Mueller"), ki18n("Original Author"), "in5y158@public.uni-ham"
         "burg.de");
     aboutData.addAuthor(ki18n("Mauricio Piacentini"), ki18n("Current maintainer"), "mauricio@tabule"
