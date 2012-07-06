67c30caa5366f59f0c383ca58656149ecb9fd6af
Christian Knippendorf <coding@christian-krippendorf.de>
2012-07-06 19:07:18 +0100

    Add MoveListAnimation source files to svn.
    svn path=/branches/work/krippendorf/kmahjongg/; revision=1304387

---
+++ MoveListAnimation.cpp
@@ -0,0 +1,92 @@
+/* Copyright (C) 2012 Christian Krippendorf <Coding@Christian-Krippendorf.de>
+ *
+ * Kmahjongg is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA. */
+
+#include "MoveListAnimation.h"
+#include "GameData.h"
+
+#include <QList>
+
+#include <KDebug>
+
+
+MoveListAnimation::MoveListAnimation(QObject * pParent)
+    : QTimer(pParent),
+    m_iAnimationSpeed(0),
+    m_iStep(0),
+    m_pGameData(NULL)
+{
+    connect(this, SIGNAL(timeout()), this, SLOT(timeoutOccurred()));
+}
+
+MoveListAnimation::~MoveListAnimation()
+{
+}
+
+void MoveListAnimation::setAnimationSpeed(int iAnimationSpeed)
+{
+    m_iAnimationSpeed = iAnimationSpeed;
+}
+
+int MoveListAnimation::getAnimationSpeed() const
+{
+    return m_iAnimationSpeed;
+}
+
+void MoveListAnimation::start(GameData * pGameData)
+{
+    m_pGameData = pGameData;
+
+    // Test whether the tileNum is max or 0.
+    if (m_pGameData->TileNum == m_pGameData->MaxTileNum) {
+        m_direction = Forward;
+    } else {
+        m_direction = Backward;
+    }
+
+    QTimer::start(m_iAnimationSpeed);
+}
+
+void MoveListAnimation::stop()
+{
+    QTimer::stop();
+
+    m_iStep = 0;
+}
+
+void MoveListAnimation::timeoutOccurred()
+{
+    if (m_pGameData == NULL) {
+        return;
+    }
+
+    if (m_direction == Forward) {
+        // Remove items...
+        emit removeItem(m_pGameData->MoveListData(m_pGameData->TileNum - 1));
+        emit removeItem(m_pGameData->MoveListData(m_pGameData->TileNum - 1));
+
+        if (m_pGameData->TileNum == 0) {
+            m_direction = Backward;
+        }
+    } else {
+        // Add items...
+        emit addItem(m_pGameData->MoveListData(m_pGameData->TileNum));
+        m_pGameData->TileNum++;
+        emit addItem(m_pGameData->MoveListData(m_pGameData->TileNum));
+        m_pGameData->TileNum++;
+
+        if (m_pGameData->TileNum == m_pGameData->MaxTileNum) {
+            m_direction = Forward;
+        }
+    }
+}
---
+++ MoveListAnimation.h
@@ -0,0 +1,94 @@
+/* Copyright (C) 2012 Christian Krippendorf <Coding@Christian-Krippendorf.de>
+ *
+ * Kmahjongg is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA. */
+
+#ifndef MOVELISTANIMATION_H
+#define MOVELISTANIMATION_H
+
+#include <QTimer>
+
+#include "KmTypes.h"
+
+
+// Forward declarations...
+class GameData;
+
+enum AnimationDirection {Forward, Backward};
+
+/**
+ * A class for a demo animation with the help of selection.
+ *
+ * @author Christian Krippendorf */
+class MoveListAnimation : public QTimer
+{
+    Q_OBJECT
+
+public:
+    /**
+     * Constructor */
+    MoveListAnimation(QObject *pParent = 0);
+
+    /**
+     * Destructor */
+    ~MoveListAnimation();
+
+    /**
+     * Set the animation speed in milliseconds.
+     *
+     * @param iAnimationSpeed The animation speed in milliseconds. */
+    void setAnimationSpeed(int iAnimationSpeed);
+
+    /**
+     * Get the animation speed in milliseconds.
+     *
+     * @return Get the animation speed in milliseconds. */
+    int getAnimationSpeed() const;
+
+    /**
+     * Override of QTimer.
+     *
+     * @param pGameData The data object to handle with for this animation process. */
+    void start(GameData * pGameData);
+
+    /**
+     * Override of QTimer. */
+    void stop();
+
+public slots:
+
+signals:
+    /**
+     * Emit to remove the given item. */
+    void removeItem(POSITION & stItem);
+
+    /**
+     * Emit to add the given item. */
+    void addItem(POSITION & stItem);
+
+private slots:
+    /**
+     * The timeout occurred. */
+    void timeoutOccurred();
+
+private:
+    int m_iStep;
+    int m_iAnimationSpeed;
+
+    AnimationDirection m_direction;
+
+    GameData * m_pGameData;
+};
+
+
+#endif // MOVELISTANIMATION_H
+
